b712f7ecbf1d20c5b1baa32783924d22f127a062
Michel Bernardo <mbpaiva@cpqd.com.br>
2025-11-29 22:54:56 -0300

    feat: adding post, put and delete endpoints.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,21 @@
 from fastapi import FastAPI
+from fastapi import HTTPException, status
+from fastapi import Response
 import uvicorn
+
+from models import TShirt
 
 
 app = FastAPI()
 
 tshirts = {
     1:{
-        "id":"0001",
+        "qte":5,
         "age":2,
         "gender":"male"
     },
     2:{
-        "id":"0002",
+        "qte":6,
         "age":1,
         "gender":"female"
     }
@@ -25,6 +29,31 @@ async def get_tshirst():
 async def get_tshirt(tshirt_id: int):
     tshirt = tshirts[tshirt_id]
     return tshirt
+
+@app.post("/tshirts", status_code=status.HTTP_201_CREATED)
+async def post_tshirt(tshirt: TShirt):
+	next_id: int = len(tshirts) + 1
+	tshirts[next_id] = tshirt
+	del tshirt.id
+	return tshirt
+
+@app.put("/tshirts/{tshirt_id}", status_code=status.HTTP_202_ACCEPTED)
+async def put_tshirt(tshirt_id: int, tshirt: TShirt):
+	if tshirt_id in tshirts:
+		tshirts[tshirt_id] = tshirt
+		del tshirt.id
+		return tshirt
+	else:
+		raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"There is no a tshirt with id {tshirt_id}")
+
+@app.delete("/tshirts/{tshirt_id}")
+async def delete_tshirt(tshirt_id: int):
+    if tshirt_id in tshirts[tshirt_id]:
+        del tshirts[tshirt_id]
+		# return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"There is no a tshirt with id {tshirt_id}")
 
 
 if __name__ == '__main__':
